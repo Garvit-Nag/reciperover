fadb335fdfdce323a22e59acb2689504385d5cb2
Gurmehar Singh Virdi <gurmeharsinghv@gmail.com>
2024-10-02 23:38:33 +0530

    Changed navbar, recommendations form tsx but not complete!

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, request, jsonify, current_app
 from app.models.recipe import Recipe
+import json
 
 api_bp = Blueprint('api', __name__)
+
+@api_bp.route('/form-data', methods=['GET'])
+def get_form_data():
+    with open('form_data.json', 'r') as file:
+        data = json.load(file)
+    return jsonify(data)
 
 @api_bp.route('/recommend', methods=['POST'])
 def recommend_recipes():
