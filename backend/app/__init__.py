ac7736b1e2187ee4415dee10d82795dbb662513c
Garvit <garvitcpp@gmail.com>
2024-10-03 04:39:42 +0530

    Reduced loading time with introduction precomputed data

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,12 +3,15 @@ from app.api.routes import api_bp
 from app.services.recommendation import FlexibleRecipeRecommendationSystem
 from config import Config
 
-def create_app(config_object=Config):  # Use Config class directly
+def create_app(config_object=Config):
     app = Flask(__name__)
     app.config.from_object(config_object)
 
-    # Initialize the recommendation system
-    app.recommendation_system = FlexibleRecipeRecommendationSystem(app.config['CSV_FILE_PATH'])
+    # Initialize the recommendation system with both CSV_FILE_PATH and PRECOMPUTED_DIR
+    app.recommendation_system = FlexibleRecipeRecommendationSystem(
+        app.config['CSV_FILE_PATH'],
+        app.config['PRECOMPUTED_DIR']
+    )
 
     app.register_blueprint(api_bp)
 
