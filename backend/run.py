b422b6cb8e4dbc78c03cf37bc4a460baab082136
Gurmehar Singh Virdi <gurmeharsinghv@gmail.com>
2024-10-04 18:11:11 +0530

    updated run.py

--- backend/run.py
+++ backend/run.py
@@ -7,11 +7,5 @@ import json
 app = create_app()
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
-@app.route('/form-data')
-def get_form_data():
-    with open('form_data.json', 'r') as file:
-        data = json.load(file)
-    return jsonify(data)
-
 if __name__ == '__main__':
     app.run(debug=True)
